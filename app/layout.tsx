65c6c25a7fd2b91956d39b4d1a399132e0b85815
seanchen0123 <270061968@qq.com>
2025-01-26 22:36:44 +0800

    nextjs版本降为14.2.5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,29 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/toaster"
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from 'next'
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "汉字书写动画",
-  description: "汉字书写动画",
-};
+  title: '汉字书写动画',
+  description: '汉字书写动画'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" className="h-full">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
-      >
+      <body className={`antialiased h-full`}>
         <ThemeProvider defaultTheme="light">
           {children}
           <Toaster />
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  devIndicators: {
-    buildActivity: false,
-  },
-};
-
-export default nextConfig;
